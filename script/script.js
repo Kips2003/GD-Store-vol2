eae573f321292d293da48b7039f7bbe130c8a2fe
Kips2003 <giorgikiparoidze07@gmail.com>
2024-09-22 16:46:39 +0400

    adding simple responsive

--- script/script.js
+++ script/script.js
@@ -1,8 +1,13 @@
+let section = document.querySelector("section");
+section.style.height = document.height-110;
+
+
 const loginButton = document.querySelector('.log-in');
 const registerButton = document.querySelector('.sign-in');
 const secTwoH2 = document.querySelector('.sec-two-wraper h2');
 const secTwoP = document.querySelector('.sec-two-wraper p');
 const secTwo = document.querySelector('.sec-two-wraper');
+const secThhree = document.querySelector('.sec-three');
 
 loginButton.style.cursor = "pointer";
 registerButton.style.cursor = "pointer";
@@ -28,6 +33,8 @@ let observer = new IntersectionObserver((entries) => {
 
 observer.observe(secTwo);
 
+observer.observe(secThree);
+
 
 const shopNowButton = document.querySelector('.shop-now');
 shopNowButton.style.cursor = "pointer";
@@ -36,3 +43,31 @@ shopNowButton.addEventListener('click', () => {
     window.location.href = 'shopping.html';
 })
 
+
+
+function scrollToSection(event, sectionId) {
+    event.preventDefault();
+    const section = document.querySelector(sectionId);
+    const offset = -110;
+    const bodyRect = document.body.getBoundingClientRect().top;
+    const sectionRect = section.getBoundingClientRect().top;
+    const sectionPosition = sectionRect - bodyRect;
+    const offsetPosition = sectionPosition + offset;
+
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+    });
+}
+const shopLinks = document.querySelectorAll('.click-shop');
+shopLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.dataset.target;
+        document.getElementById(targetId).style.padding = '30px';
+        setTimeout(() =>{
+            document.getElementById(targetId).style.padding = '20px';
+        },150);
+        
+    });
+});
